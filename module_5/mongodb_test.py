1a66989b5b4e1e9f3ee6f45e66386e8655e41773
bbordwell <benbordwell@hotmail.com>
2021-11-11 17:42:59 -0600

    Add comments

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,3 +1,6 @@
+#Ben Bordwell Module 5.2 assignment 11/11/2021
+#This program tests conecting to a MongoDB and displays the collections it contains.
+
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.ckvk8.mongodb.net/students?retryWrites=true&w=majority"
